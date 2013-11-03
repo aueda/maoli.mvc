454f9796f884ee36f90b2d2da65ef59c45ed2725
Adriano Ueda <adriano.ueda@gmail.com>
2013-11-03 21:27:07 -0200

    Included conditional validation of CPF and CNPJ.
    This conditional validation checks if a field has a specific value to validate the CPF or CNPJ.

---
+++ Maoli.Mvc/CpfIfAttribute.cs
@@ -0,0 +1,69 @@
+﻿namespace Maoli.Mvc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+    using Maoli;
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public sealed class CpfIfAttribute : ValidationAttribute, IClientValidatable
+    {
+        private CpfPunctuation punctuation = CpfPunctuation.Loose;
+
+        private string testProperty = string.Empty;
+
+        private string testPropertyValue = string.Empty;
+
+        public CpfIfAttribute()
+            : this("O CPF é inválido", string.Empty, string.Empty)
+        {
+        }
+
+        public CpfIfAttribute(string testProperty, string testPropertyValue)
+            : base()
+        {
+            this.testProperty = testProperty;
+            this.testPropertyValue = testPropertyValue;
+        }
+
+        public CpfIfAttribute(string errorMessage, string testProperty, string testPropertyValue)
+            : base(errorMessage)
+        {
+            this.testProperty = testProperty;
+            this.testPropertyValue = testPropertyValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var valueAsString = value as string;
+
+            if (string.IsNullOrWhiteSpace(valueAsString))
+            {
+                return true;
+            }
+
+            var isValid = Cpf.Validate(valueAsString, this.punctuation);
+
+            return isValid;
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(
+            ModelMetadata metadata,
+            ControllerContext context)
+        {
+            var punctuationName =
+                Enum.GetName(typeof(CpfPunctuation), this.punctuation).ToLower();
+
+            var rule = new ModelClientValidationRule();
+
+            rule.ValidationType = "cpfif";
+            rule.ErrorMessage = this.FormatErrorMessage(metadata.GetDisplayName());
+            rule.ValidationParameters.Add("punctuation", punctuationName);
+            rule.ValidationParameters.Add("testproperty", testProperty);
+            rule.ValidationParameters.Add("testpropertyvalue", testPropertyValue);
+
+            yield return rule;
+        }
+    }
+}
--- Maoli.Mvc/Scripts/jquery.validate.maoli.js
+++ Maoli.Mvc/Scripts/jquery.validate.maoli.js
@@ -10,7 +10,7 @@
     var maoli = window.Maoli,
         $ = window.jQuery,
         validator = null,
-		addCepMethod = function() {
+		addCepMethod = function () {
 		    validator.addMethod("cep_validator", function (value, element) {
 
 		        var punctuation = $(element).data("val-cep-punctuation") || "loose";
@@ -19,7 +19,53 @@
 		    });
 
 		    validator.unobtrusive.adapters.addBool('cep', 'cep_validator');
-		};
+		},
+        addCpfIfMethod = function () {
+            validator.addMethod("cpfif_validator", function (value, element) {
+
+                var punctuation = $(element).data("val-cpfif-punctuation") || "loose",
+                    testProperty = $(element).data("val-cpfif-testproperty"),
+                    testPropertyValue = $(element).data("val-cpfif-testpropertyvalue").toString(),
+                    isValid = maoli.Cpf.validate(value, punctuation);
+
+				if (value === "") {
+					return true;
+				}
+
+                if (!!testProperty) {
+                    if ($("#" + testProperty).val() !== testPropertyValue) {
+                        isValid = true;
+                    }
+                }
+
+                return isValid;
+            });
+
+            validator.unobtrusive.adapters.addBool('cpfif', 'cpfif_validator');
+        },
+        addCnpjIfMethod = function () {
+            validator.addMethod("cnpjif_validator", function (value, element) {
+
+                var punctuation = $(element).data("val-cnpjif-punctuation") || "loose",
+                    testProperty = $(element).data("val-cnpjif-testproperty"),
+                    testPropertyValue = $(element).data("val-cnpjif-testpropertyvalue").toString(),
+                    isValid = maoli.Cnpj.validate(value, punctuation);
+
+				if (value === "") {
+					return true;
+				}
+
+                if (!!testProperty) {
+                    if ($("#" + testProperty).val() !== testPropertyValue) {
+                        isValid = true;
+                    }
+                }
+
+                return isValid;
+            });
+
+            validator.unobtrusive.adapters.addBool('cnpjif', 'cnpjif_validator');
+        };
 
     if (maoli === "undefined" || $ === "undefined" || $.validator === "undefined") {
         return;
@@ -28,6 +74,10 @@
     validator = $.validator;
 
     addCepMethod();
+
+    addCpfIfMethod();
+
+    addCnpjIfMethod();
 
     validator.addMethod("cnpj_validator", function (value, element) {
 
@@ -46,7 +96,5 @@
     validator.unobtrusive.adapters.addBool('cnpj', 'cnpj_validator');
 
     validator.unobtrusive.adapters.addBool('cpf', 'cpf_validator');
-
-
 
 }(this));
